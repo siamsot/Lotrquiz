4bda92e95bb25d23fecc81e25e21649d860c86b2
Siamsot <siamsot@gmail.com>
2017-07-18 14:51:29 +0100

    Fixed bug in question 3

--- app/src/main/java/com/siamsot/lotrquiz/MainActivity.java
+++ app/src/main/java/com/siamsot/lotrquiz/MainActivity.java
@@ -28,11 +28,16 @@ public void submitScore (View view){
     CheckBox legolas = (CheckBox) findViewById(R.id.legolas);
     boolean question3b = legolas.isChecked();
 
+    CheckBox elrond = (CheckBox) findViewById(R.id.elrond);
+    boolean question3c = elrond.isChecked();
+
     EditText password = (EditText) findViewById(R.id.moria_answer);
     String moria_pass = password.getText().toString();
+    moria_pass = moria_pass.trim();
 
     EditText surname = (EditText) findViewById(R.id.frodo_answer);
     String frodo_surname = surname.getText().toString();
+    frodo_surname = frodo_surname.trim();
 
     RadioButton nazgul = (RadioButton) findViewById(R.id.nazguls_right);
     boolean question5 = nazgul.isChecked();
@@ -40,14 +45,14 @@ public void submitScore (View view){
     RadioButton ring = (RadioButton) findViewById(R.id.ring_true);
     boolean question6 = ring.isChecked();
 
-    Score = calculateScore(question1, moria_pass, question3a, question3b, frodo_surname, question5, question6);
+    Score = calculateScore(question1, moria_pass, question3a, question3b, question3c, frodo_surname, question5, question6);
     if (Score > 3)
     Toast.makeText(this, "Well done! Your final score is " + Score, Toast.LENGTH_LONG).show();
     else {
         Toast.makeText(this, "It would be better to read the books again! Your final score is " + Score, Toast.LENGTH_LONG).show();
     }
 }
-private int calculateScore (boolean q1, String q2, boolean q3a, boolean q3b, String q4, boolean q5, boolean q6) {
+private int calculateScore (boolean q1, String q2, boolean q3a, boolean q3b, boolean q3c, String q4, boolean q5, boolean q6) {
     int Score2 = 0;
     if (q1){
         Score2 = Score2 + 1;
@@ -55,7 +60,7 @@ private int calculateScore (boolean q1, String q2, boolean q3a, boolean q3b, Str
     if (Objects.equals(q2, "Mellon")) {
         Score2 = Score2 + 1;
     }
-    if (q3a && q3b){
+    if (q3a && q3b && !q3c){
         Score2 = Score2 + 1;
     }
     if (Objects.equals(q4, "Baggins")){
